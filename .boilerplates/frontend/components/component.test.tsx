ecad80b4e9fd4dc723a201e8f95edfe2f2548dd4
trncngh <hautrancong@gmail.com>
2024-11-05 07:38:31 +0700

    add test boilerplate

---
+++ .boilerplates/frontend/components/component.test.tsx
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom'
+import { cleanup, fireEvent, render, screen } from '@testing-library/react'
+import { vi } from 'vitest'
+import {{fileName}} from './{{fileName}}'
+
+describe('{{fileName}} Component', () => {
+    const handleClose = vi.fn()
+    afterEach(cleanup)
+
+})
